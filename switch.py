64582f6e07439793147f1c1f0a722b4f0174a849
dariusstefan <darius.stefan.02@gmail.com>
2023-11-16 01:02:30 +0200

    Implemented forwarding with learning.

--- switch.py
+++ switch.py
@@ -5,6 +5,114 @@ import wrapper
 import threading
 import time
 from wrapper import recv_from_any_link, send_to_link, get_switch_mac, get_interface_name
+from enum import Enum, auto
+
+class SwitchState(Enum):
+    STATE_INIT = 0
+    STATE_LISTENING = auto()
+    STATE_FORWARD = auto()
+
+class InstanceData:
+    def __init__(self):
+        CAM_table = None
+        num_interfaces = None
+        current_dest_mac = None
+        current_src_mac = None
+        current_ethertype = None
+        input_interface = None
+        packet_data = None
+        packet_length = None
+
+def do_state_init(instance_data):
+    # init returns the max interface number. Our interfaces
+    # are 0, 1, 2, ..., init_ret value + 1
+    switch_id = sys.argv[1]
+
+    num_interfaces = wrapper.init(sys.argv[2:])
+    interfaces = range(0, num_interfaces)
+
+    print("# Starting switch with id {}".format(switch_id), flush=True)
+    print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
+
+    # Create and start a new thread that deals with sending BDPU
+    t = threading.Thread(target=send_bdpu_every_sec)
+    t.start()
+
+    # Printing interface names
+    for i in interfaces:
+        print(get_interface_name(i))
+
+    instance_data.CAM_table = {}
+    instance_data.num_interfaces = num_interfaces
+    return SwitchState.STATE_LISTENING
+
+def do_state_listening(instance_data):
+    # Note that data is of type bytes([...]).
+    # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
+    # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
+    # b3 = b1[0:2] + b[3:4].
+    interface, data, length = recv_from_any_link()
+
+    dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
+
+    instance_data.current_dest_mac = dest_mac
+    instance_data.current_src_mac = src_mac
+
+    # Print the MAC src and MAC dst in human readable format
+    print("Source MAC: ", end='')
+    print_mac(src_mac)
+    print("Destination MAC: ", end='')
+    print_mac(dest_mac)
+
+    # Note. Adding a VLAN tag can be as easy as
+    # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
+
+    print(f'EtherType: {ethertype}')
+
+    print("Received frame of size {} on interface {}".format(length, interface), flush=True)
+
+    print("CAM table:")
+    print_cam_table(instance_data.CAM_table)
+    print("-------------------------")
+
+    instance_data.current_ethertype = ethertype
+    instance_data.input_interface = interface
+    instance_data.packet_data = data
+    instance_data.packet_length = length
+
+    # TODO: Implement forwarding with learning
+    # TODO: Implement VLAN support
+    # TODO: Implement STP support
+
+    # data is of type bytes.
+    # send_to_link(i, data, length)
+    return SwitchState.STATE_FORWARD
+
+def do_state_forward(instance_data):
+    instance_data.CAM_table[instance_data.current_src_mac] = instance_data.input_interface
+    if is_unicast(instance_data.current_dest_mac):
+        if instance_data.current_dest_mac in instance_data.CAM_table:
+            send_to_link(instance_data.CAM_table[instance_data.current_dest_mac], instance_data.packet_data, instance_data.packet_length)
+        else:
+            for i in range(0, instance_data.num_interfaces):
+                if i != instance_data.input_interface:
+                    send_to_link(i, instance_data.packet_data, instance_data.packet_length)
+    else:
+        for i in range(0, instance_data.num_interfaces):
+            if i != instance_data.input_interface:
+                send_to_link(i, instance_data.packet_data, instance_data.packet_length)
+
+    return SwitchState.STATE_LISTENING
+
+state_functions = {
+    SwitchState.STATE_INIT: do_state_init,
+    SwitchState.STATE_LISTENING: do_state_listening,
+    SwitchState.STATE_FORWARD: do_state_forward
+}
+
+def run_state(state, instance_data):
+    return state_functions[state](instance_data)
+
 
 def parse_ethernet_header(data):
     # Unpack the header fields from the byte array
@@ -34,53 +142,25 @@ def send_bdpu_every_sec():
         # TODO Send BDPU every second if necessary
         time.sleep(1)
 
+def is_unicast(mac):
+    first_byte = mac[0]
+    return (first_byte & 0x01) == 0
+
+def print_mac(mac):
+    print(':'.join(f'{b:02x}' for b in mac))
+
+def print_cam_table(cam_table):
+    for key, value in cam_table.items():
+        print(f"MAC: {':'.join(f'{b:02x}' for b in key)}, Interface: {value}")
+
+instance_data = InstanceData()
+
 def main():
-    # init returns the max interface number. Our interfaces
-    # are 0, 1, 2, ..., init_ret value + 1
-    switch_id = sys.argv[1]
-
-    num_interfaces = wrapper.init(sys.argv[2:])
-    interfaces = range(0, num_interfaces)
-
-    print("# Starting switch with id {}".format(switch_id), flush=True)
-    print("[INFO] Switch MAC", ':'.join(f'{b:02x}' for b in get_switch_mac()))
-
-    # Create and start a new thread that deals with sending BDPU
-    t = threading.Thread(target=send_bdpu_every_sec)
-    t.start()
-
-    # Printing interface names
-    for i in interfaces:
-        print(get_interface_name(i))
+    current_state = SwitchState.STATE_INIT
 
     while True:
-        # Note that data is of type bytes([...]).
-        # b1 = bytes([72, 101, 108, 108, 111])  # "Hello"
-        # b2 = bytes([32, 87, 111, 114, 108, 100])  # " World"
-        # b3 = b1[0:2] + b[3:4].
-        interface, data, length = recv_from_any_link()
+        current_state = run_state(current_state, instance_data)
 
-        dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
-
-        # Print the MAC src and MAC dst in human readable format
-        dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
-        src_mac = ':'.join(f'{b:02x}' for b in src_mac)
-
-        # Note. Adding a VLAN tag can be as easy as
-        # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
-
-        print(f'Destination MAC: {dest_mac}')
-        print(f'Source MAC: {src_mac}')
-        print(f'EtherType: {ethertype}')
-
-        print("Received frame of size {} on interface {}".format(length, interface), flush=True)
-
-        # TODO: Implement forwarding with learning
-        # TODO: Implement VLAN support
-        # TODO: Implement STP support
-
-        # data is of type bytes.
-        # send_to_link(i, data, length)
 
 if __name__ == "__main__":
     main()
